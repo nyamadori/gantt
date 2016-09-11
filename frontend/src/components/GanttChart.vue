ff47e44eb131e677edd38e7ac02b8831aa808d67
nyamadori <nyamadorig@gmail.com>
2016-09-11 18:58:36 +0900

    Create ScheduleTable component

--- frontend/src/components/GanttChart.vue
+++ frontend/src/components/GanttChart.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="gantt-chart">
     <schedule-title-list :schedules="schedules"></schedule-title-list>
-
-    <div class="schedule-table" @scroll="onScroll">
-      <schedule-table-header></schedule-table-header>
-      <schedule-table-row
-        v-for="schedule in schedules | orderBy compareSchedule"
-        :schedule="schedule"></schedule-table-row>
-    </div>
+    <schedule-table :schedule="schedules"></schedule-table>
   </div>
 </template>
 
@@ -31,24 +25,18 @@
 </style>
 
 <script>
+import ScheduleTable from './ScheduleTable'
 import ScheduleTitleList from './ScheduleTitleList'
-import ScheduleTableRow from './ScheduleTableRow'
-import ScheduleTableHeader from './ScheduleTableHeader'
-import ScheduleComparable from '../mixins/ScheduleComparable'
-import { schedules, table } from '../vuex/getters'
-import { setTable } from '../vuex/actions'
+import { schedules } from '../vuex/getters'
 
 export default {
-  mixins: [ ScheduleComparable ],
-
-  vuex: {
-    getters: { schedules, table },
-    actions: { setTable }
-  },
   components: {
     ScheduleTitleList,
-    ScheduleTableRow,
-    ScheduleTableHeader
+    ScheduleTable
+  },
+
+  vuex: {
+    getters: { schedules }
   },
 
   methods: {
