6a02619b250ed13e405f535a1125856e04695033
nyamadori <nyamadorig@gmail.com>
2016-09-29 01:13:35 +0900

    Register DatePicker component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App'
+import DatePicker from './components/DatePicker'
+
+Vue.component('date-picker', DatePicker)
 
 /* eslint-disable no-new */
 new Vue({
