0e7b5beee82f5f1d381ead21d7362191a73f8ddb
nyamadori <nyamadorig@gmail.com>
2016-10-03 16:55:21 +0900

    Add DateInputWithCalendar component

---
+++ frontend/src/components/DateInputWithCalendar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="date-input-with-calendar">
+    <date-input></date-input>
+    <calendar></calendar>
+  </div>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import Calendar from './components/Calendar'
 import DateInput from './components/DateInput'
+import DateInputWithCalendar from './components/DateInputWithCalendar'
 
 Vue.component('date-input', DateInput)
 Vue.component('calendar', Calendar)
+Vue.component('date-input-with-calendar', DateInputWithCalendar)
 
 /* eslint-disable no-new */
 new Vue({
