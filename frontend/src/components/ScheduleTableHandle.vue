da8989c83701fa2fa9acc7457a3492c82eab596e
nyamadori <nyamadorig@gmail.com>
2016-10-21 15:56:50 +0900

    Remove debug log

--- frontend/src/components/ScheduleTableHandle.vue
+++ frontend/src/components/ScheduleTableHandle.vue
@@ -32,12 +32,6 @@ export default {
     getters: { table, tableCell, handle }
   },
 
-  watch: {
-    date (v) {
-      console.log('watch', v)
-    }
-  },
-
   data () {
     return {
       _date: null,
